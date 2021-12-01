a134c626f7526b792f7ce7426d41a169a1919545
Keith L <keithl@radontech.xyz>
2021-12-01 00:41:43 -0500

    day 1 done, needs cleanup

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 My solutions to the 2021 Advent of Code Challenge
 
-| Day           | Name                      | Concept                      | Language/Notes |
-| :-----------: | ------------------------- | ---------------------------- | -------------- |
-| [1](day_1)    | Some name for this puzzle | Some concept for this puzzle | Rust? idk      |
+| Day           | Name                      | Concept                           | Language/Notes |
+| :-----------: | ------------------------- | ------------------------------    | -------------- |
+| [1](day_1)    | Sonar Sweep               | Comparator & 3-Pair Comparator    | Rust           |
--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -12,6 +12,30 @@ fn main() {
     //let organized = input.split('/').collect::<Vec<&str>>(); // split by arg into &str
     //let organized = input.split_whitespace().collect::<Vec<&str>>(); // split by whitespace into &str
     println!("{:?}", organized);
+    
+    //PARSE TO INTEGER
+    let mut nums = vec![];
+    for i in organized {
+        nums.push(i.parse::<i32>().unwrap());
+    }
+   println!("{:?}", nums);
+
+   let mut a = 0;
+   let mut b = 0;
+   let mut sum = 0;
+
+   let index = 3;
+   for i in index..nums.len() {
+        a = nums[i-1] + nums[i-2] + nums[i-3];
+        b = nums[i] + nums[i-1] + nums[i-2];
+        if b > a {
+            sum += 1;
+        }
+   }
+
+   println!("{}", sum);
+
+
 
 }
 
